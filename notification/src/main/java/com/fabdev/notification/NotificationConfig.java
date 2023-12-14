ac09a95e145702c075218eb811f9c9e687dd48fc
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2023-12-14 20:35:35 +0100

    Message producer configure in the amqp module

--- notification/src/main/java/com/fabdev/notification/NotificationConfig.java
+++ notification/src/main/java/com/fabdev/notification/NotificationConfig.java
@@ -1,6 +1,11 @@
 package com.fabdev.notification;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -9,11 +14,29 @@ public class NotificationConfig {
     @Value("${rabbitmq.exchanges.internal}")
     private String internalExchange;
 
-    @Value("${rabbitmq.queue.notification}")
+    @Value("${rabbitmq.queues.notification}")
     private String notificationQueue;
 
     @Value("${rabbitmq.routing-keys.internal-notification}")
     private String internalNotificationRoutingKey;
+
+    @Bean
+    public TopicExchange internalTopicExchange(){
+        return new TopicExchange(this.internalExchange);
+    }
+
+    @Bean
+    public Queue notificationQueue(){
+        return new Queue(this.notificationQueue);
+    }
+
+    @Bean
+    public Binding internalToNotificationBinding(){
+        return BindingBuilder
+                .bind(notificationQueue())
+                .to(internalTopicExchange())
+                .with(internalNotificationRoutingKey);
+    }
 
     public String getInternalExchange() {
         return internalExchange;
