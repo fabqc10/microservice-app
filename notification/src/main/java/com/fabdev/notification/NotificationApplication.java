bcd1aaa30058d82b65415f4b3bc9cd12fd4f7618
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2023-12-14 21:19:32 +0100

    runner in notification application to publish a message and rabbit address in in notification yml

--- notification/src/main/java/com/fabdev/notification/NotificationApplication.java
+++ notification/src/main/java/com/fabdev/notification/NotificationApplication.java
@@ -1,13 +1,35 @@
 package com.fabdev.notification;
 
+import com.fabdev.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.fabdev.notification",
+                "com.fabdev.amqp"
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class,args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer,
+//                                        NotificationConfig notificationConfig) {
+//        return args -> {
+//            producer.publish(
+//                    new Person("Jonny Doe",25),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey()
+//            );
+//        };
+//    }
+//
+//    record Person(String name, int age){}
 }
