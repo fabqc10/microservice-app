31e805f4a1cbe0d6c981ffceeceba0de5bb7a0e9
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2023-12-21 22:42:02 +0100

    refactores feign

--- clients/src/main/java/com/fabdev/clients/fraud/FraudClient.java
+++ clients/src/main/java/com/fabdev/clients/fraud/FraudClient.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 // reminder: If any microservice wants to use this client just need to invoke this module in the POM & in the App class
-@FeignClient("fraud")
+//@FeignClient("fraud")
+@FeignClient(
+        name = "fraud",
+        url = "${clients.fraud.url}"
+)
 public interface FraudClient {
 
     @GetMapping(path = "api/v1/fraud-check/{customerId}")
--- customer/src/main/java/com/fabdev/customer/CustomerApplication.java
+++ customer/src/main/java/com/fabdev/customer/CustomerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 // The reason to do this is to have the ability to inject RabbitMQMessageProducer
 @SpringBootApplication(
@@ -16,6 +18,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients(
         basePackages = "com.fabdev.clients"
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class,args);
